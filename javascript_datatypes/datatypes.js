7299f1b60e31706c96ab78f85a6366c0c00edd66
Pranshu-pc12 <pranshuchauhan8@gmail.com>
2025-02-25 13:39:48 +0000

    memory types in javascript

--- javascript_datatypes/datatypes.js
+++ javascript_datatypes/datatypes.js
@@ -7,3 +7,30 @@
 // 6 - Undifinted
 // 7 - Symbol
 
+// memory types in javascript
+// 1 -Stack - All privmitive datatypes are resirive Stack memory. Stack memory is provide a copy of data for change but real data is never change;
+    
+//    2- Heap Memory - All non primitive datatypes (object) are resirive heap memory. Heap memory is provide a refrence (Hard copy) for change 
+
+
+let name = "pranshu chauhan";
+
+let newname = name;
+newname = "Shivam Chauhan"
+
+console.log(name)
+console.log(newname)
+
+let mydetails = {
+    name:"Pranshu chauhan",
+    age: 23,
+    school: "narayan Vidya Ashram"
+
+}
+console.log(mydetails.name)
+let newDetails = mydetails;
+newDetails.name = "Surya Pratap singh"
+console.log(mydetails.name)
+console.log(newDetails.name)
+
+
